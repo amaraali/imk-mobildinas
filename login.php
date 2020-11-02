edd98bccfc9fc3dfb567681a0b157eace2138d9b
amara <amara@students.unnes.ac.id>
2020-11-02 21:03:46 +0700

    nambah favicon

--- login.php
+++ login.php
@@ -72,7 +72,7 @@ if(isset($_POST["login"])){
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
